f5b0743071b4e5ab390eda7b60d16bf3d4e613f2
sekarfeb <sekarfeb@gmail.com>
2023-08-01 16:05:28 +0530

    my files

---
+++ frontend/src/pages/Landing.js
@@ -0,0 +1,38 @@
+import main from '../assets/images/main.svg';
+import Wrapper from '../assets/wrappers/LandingPage';
+import { Logo } from '../components';
+import { Link } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
+import { useAppContext } from '../context/appContext';
+import React from 'react';
+
+const Landing = () => {
+  const { user } = useAppContext();
+  return (
+    <React.Fragment>
+      {user && <Navigate to='/' />}
+      <Wrapper>
+        <nav>
+          <Logo />
+        </nav>
+        <div className='container page'>
+          {/* info */}
+          <div className='info'>
+            <h1>
+              job <span>tracking</span> app
+            </h1>
+            <p>
+             Online HR Job Portal for Software company Registered
+            </p>
+            <Link to='/register' className='btn btn-hero'>
+              Login/Register
+            </Link>
+          </div>
+          <img src={main} alt='job hunt' className='img main-img' />
+        </div>
+      </Wrapper>
+    </React.Fragment>
+  );
+};
+
+export default Landing;
